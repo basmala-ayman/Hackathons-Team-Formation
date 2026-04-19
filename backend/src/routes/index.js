2070737ee451cd02067e6b4ae3e8bc1121cfde89
Habiba Abdelgowad Habiba-Adel <habibaabdelgowad9@gmail.com>
2026-04-19 23:09:51 +0200

    making the API contract that closes #14 issue

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,8 +5,12 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("../modules/user/user.routes");
-
+const authRoutes= require("../modules/auth/auth.routes");
 //it making error so we will comment it until there is things be written there 
 //router.use("/users", userRoutes);
+
+
+
+router.use("/auth",authRoutes);
 
 module.exports = router;
