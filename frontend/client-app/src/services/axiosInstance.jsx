8808ad027c9c46fe075214de9eace50a9d55484f
Basmala Ayman <basmala.ayman.1904@gmail.com>
2026-04-22 21:11:21 +0200

    Add authService Logic

---
+++ frontend/client-app/src/services/axiosInstance.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import {
+  clearTokens,
+  getAccessToken,
+  getRefreshToken,
+  storeTokens,
+} from "../utils/tokenStorage";
+
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// for refresh token process
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = getRefreshToken();
+        const response = await axios.post(
+          `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
+          { refreshToken }
+        );
+        const { accessToken, refreshToken: newRefreshToken } =
+          response.data.data;
+
+        storeTokens(accessToken, newRefreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        // if refresh fails (refreshToken expired)
+        clearTokens();
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
